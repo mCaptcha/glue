39e2ae434437d2193effc0d7a61b200fa1f62190
realaravinth <realaravinth@batsense.net>
2021-12-15 14:14:40 +0530

    globalObject 'self' was causing errors when run in browser. So changing to 'this'
    https://stackoverflow.com/questions/64639839/typescript-webpack-library-generates-referenceerror-self-is-not-defined

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -1,29 +1,29 @@
-('use strict');
-const path = require('path');
+("use strict");
+const path = require("path");
 
-const dist = path.resolve(__dirname, 'dist');
+const dist = path.resolve(__dirname, "dist");
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: "source-map",
   //mode: 'development',
-  mode: 'production',
-  entry: './src/index.ts',
+  mode: "production",
+  entry: "./src/index.ts",
   output: {
     path: dist,
-    filename: 'index.js',
-    libraryTarget: 'umd',
-    library: 'mcaptchaGlue'
+    filename: "index.js",
+    libraryTarget: "umd",
+    globalObject: "this",
+    library: "mcaptchaGlue",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
       },
-
-      ],
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
   },
 };
